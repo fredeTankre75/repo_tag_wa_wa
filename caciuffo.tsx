ddf904bece01e09bf85496dc9117b24747677735
Boredoutofpikiran4091 <federico.pistorio96@gmail.com>
2022-03-01 13:07:09 +0100

    xxx commit zozzi xxx

---
+++ caciuffo.tsx
@@ -0,0 +1,14 @@
+/*
+tano facciamo che ripassiamo typescript? 
+magari mi aiuti anche in ionic?????
+pls, ty.
+*/
+
+
+interface person{
+    name:string;
+    age:Number;
+    id:number
+    address:string;
+    addressNumber:Number
+}
